3ea332da6bae198f680c5896e0f2644de3a3182a
Rangga Rifqi <rangga@ranggarifqi.com>
2024-06-04 08:41:36 +0700

    Added supabase client

---
+++ helper/supabase.go
@@ -0,0 +1,16 @@
+package helper
+
+import (
+	"log"
+	"os"
+
+	"github.com/supabase-community/supabase-go"
+)
+
+func GetClient() *supabase.Client {
+	client, err := supabase.NewClient(os.Getenv("SUPABASE_PROJECT_URL"), os.Getenv("SUPABASE_API_KEY"), nil)
+	if err != nil {
+		log.Fatalf("Failed to create Supabase client: %s", err)
+	}
+	return client
+}
